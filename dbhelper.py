ef28a0f515015dbc5bb27f629db630fa171ab274
Shrish Kamboj <shrishkamboz@gmail.com>
2025-01-15 12:38:39 +0530

    app-upd

--- dbhelper.py
+++ dbhelper.py
@@ -1,13 +1,15 @@
 import mysql.connector
+import sys
 
 class dbhelper:
 
     def __init__(self):
         try:
-            self.conn=mysql.connector.connect(host="localhost",username="root",password="",database='test-db')
+            self.conn=mysql.connector.connect(host="localhost",user="root",password="",database='test-db')
             self.mycursor = self.conn.cursor()
         except:
-            print("Connection Successfull")
-        else:
             print("Oh NO! , Some error occured.")
+            sys.exit(0)
+        else:
+            print("Connection Succesfull")
 
